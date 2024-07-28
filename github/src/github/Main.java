61425e7ae5204293d8c06dac6f20ed97554f1274
nisar <nisar@Nisarg>
2024-07-28 09:22:44 +0530

    added other branch

--- github/src/github/Main.java
+++ github/src/github/Main.java
@@ -4,5 +4,6 @@ public class Main {
 public static void main(String[] args) {
 	System.out.println("Frist github program");
 	System.out.println("second line of  program");
+	System.out.println("other brach code");
 }
 }
